681665847500443e6d3db56f64447a2a5a4ae055
ktejas <krishnasalimeda@gmail.com>
2015-11-30 23:25:51 -0700

    Initial commit for solution 014

---
+++ 014_Chocolate Feast/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t;
+	for (int a0 = 0; a0 < t; a0++)
+	{
+		int n;
+		int c;
+		int m;
+		cin >> n >> c >> m;
+	}
+	return 0;
+}
