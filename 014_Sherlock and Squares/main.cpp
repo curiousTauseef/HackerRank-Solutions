a26ebce43423ce233f29c83bf94d04985f22aa4c
ktejas <krishnasalimeda@gmail.com>
2016-01-01 17:06:29 -0700

    Added solution 14

--- 014_Sherlock and Squares/main.cpp
+++ 014_Sherlock and Squares/main.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
+
+int PrimesCount(int, int);
 
 int main()
 {
 	int t;
 	int a, b;
 
-	for (int t = 0;)
+	cin >> t;
 
-		return 0;
+	for (int i = 0; i < t; i++)
+	{
+		cin >> a >> b;
+		cout << PrimesCount(a,b) << endl;
+	}
+
+	system("PAUSE");
+	return 0;
+}
+
+int PrimesCount(int i_a, int i_b)
+{
+	return floor(sqrt(i_b)) - ceil(sqrt(i_a)) + 1;
 }
